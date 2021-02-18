80498a14c4650d9e3431a897070d5b2823c3d767
Ngoni-Sama <60420709+Ngoni-Sama@users.noreply.github.com>
2021-02-18 17:07:15 +0200

    .

--- lib/otp.dart
+++ lib/otp.dart
@@ -35,7 +35,7 @@ class _OTPScreenState extends State<OTPScreen> {
             margin: EdgeInsets.only(top: 40),
             child: Center(
               child: Text(
-                'Verify +${widget.phone}',
+                'Please type verification code sent to +${widget.phone}',
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
               ),
             ),
